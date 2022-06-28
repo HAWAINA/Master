8eed627a42253a7aa15dc8ba01ae17d762e96114
HAWAINA <ismettrabzon08@gmail.com>
2022-06-28 20:25:32 +0600

    Employee commit

--- business/views.py
+++ business/views.py
@@ -42,6 +42,32 @@ def employee_creation_view(request):
         return Response(data=EmployeeCreationSerializer(employee_create).data)
 
 
+@api_view(["GET", "PUT", "DELETE"])
+def employee_refactor_view(request, id):
+    try:
+        employee_create = EmployeeCreation.objects.get(id=id)
+    except EmployeeCreation.DoesNotExist:
+        return Response(
+            data={"error": "Employee not Found!!!"}, status=status.HTTP_404_NOT_FOUND
+        )
+    if request.method == "GET":
+        serializer = EmployeeCreationSerializer(employee_create, many=True)
+        return Response(data=serializer.data)
+    elif request.method == "DELETE":
+        employee_create.delete()
+        return Response(data={"massage": "Employee removed!"})
+    else:
+        employee_create.profile = request.data.get("profile")
+        employee_create.nickname = request.data.get("nickname")
+        employee_create.tel_number = request.data.get("tel_number")
+        employee_create.service_specialty = request.data.get("service_specialty")
+        employee_create.length_of_service = request.data.get("length_of_service")
+        employee_create.description = request.data.get("description")
+        employee_create.calendar_refactor = request.data.get("calendar_refactor")
+        employee_create.save()
+        return Response(data=EmployeeCreationSerializer(employee_create).data)
+
+
 @api_view(["GET", "POST"])
 def calendar_registration_view(request):
     try:
@@ -70,33 +96,6 @@ def calendar_registration_view(request):
         return Response(data=CalendarRegistrationSerializer(calendar_create).data)
 
 
-# Запросы по редактирование мастера
-@api_view(["GET", "PUT", "DELETE"])
-def employee_refactor_view(request, id):
-    try:
-        employee_create = EmployeeCreation.objects.get(id=id)
-    except EmployeeCreation.DoesNotExist:
-        return Response(
-            data={"error": "Employee not Found!!!"}, status=status.HTTP_404_NOT_FOUND
-        )
-    if request.method == "GET":
-        serializer = EmployeeCreationSerializer(employee_create, many=True)
-        return Response(data=serializer.data)
-    elif request.method == "DELETE":
-        employee_create.delete()
-        return Response(data={"massage": "Employee removed!"})
-    else:
-        employee_create.profile = request.data.get("profile_ref")
-        employee_create.nickname = request.data.get("nickname_ref")
-        employee_create.tel_number = request.data.get("tel_number_ref")
-        employee_create.service_specialty = request.data.get("service_specialty_ref")
-        employee_create.length_of_service = request.data.get("length_of_service_ref")
-        employee_create.description = request.data.get("description_ref")
-        employee_create.calendar_refactor = request.data.get("calendar_refactor")
-        employee_create.save()
-        return Response(data=EmployeeCreationSerializer(employee_create).data)
-
-
 @api_view(["GET", "PUT", "DELETE"])
 def calendar_refactor_view(request):
     try:
@@ -112,10 +111,10 @@ def calendar_refactor_view(request):
         calendar_registration.delete()
         return Response(data={"massage": "Calendar removed!"})
     else:
-        calendar_registration.work_calendar = request.data.get("work_calendar_ref")
-        calendar_registration.getting_started = request.data.get("getting_started_ref")
-        calendar_registration.end_of_job = request.data.get("end_of_job_ref")
-        calendar_registration.lunch_start = request.data.get("lunch_start_ref")
-        calendar_registration.lunch_end = request.data.get("lunch_end_ref")
+        calendar_registration.work_calendar = request.data.get("work_calendar")
+        calendar_registration.getting_started = request.data.get("getting_started")
+        calendar_registration.end_of_job = request.data.get("end_of_job")
+        calendar_registration.lunch_start = request.data.get("lunch_start")
+        calendar_registration.lunch_end = request.data.get("lunch_end")
         calendar_registration.save()
         return Response(data=CalendarRegistrationSerializer(calendar_registration).data)
